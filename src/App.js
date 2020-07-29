56a543e232afe72734a37cd39c8f4fb199e34b68
meeshh <mick.f4rr3ll@gmail.com>
2020-07-29 16:39:57 +0300

    localization

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+import { withTranslation, Trans } from 'react-i18next'
+
+function App(props) {
+  const [state, setState] = useState({ value: 'en' })
+
+  const onLanguageHandle = (event) => {
+    let newLang = event.target.value;
+    setState({ value: newLang })
+    props.i18n.changeLanguage(newLang)
+  }
+
+  const renderRadioButtons = () => {
+    return (<div>
+      <input
+        checked={state.value === 'en'}
+        name="language"
+        onChange={(e) => onLanguageHandle(e)}
+        value="en"
+        type="radio" />English &nbsp;
+      <input
+        name="language"
+        value="ar"
+        checked={state.value === 'ar'}
+        type="radio"
+        onChange={(e) => onLanguageHandle(e)}
+      />Arabic
+    </div>)
+  }
+
+  const { t } = props
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {renderRadioButtons()}
+      <h1><Trans>Paragraph</Trans></h1>
+      <table>
+        <tbody>
+          <tr>
+            <td style={{ width: '20%' }}>
+              {t('author.title')}
+            </td>
+            <td style={{ width: '5%' }}>:</td>
+            <td style={{ width: '75%' }}>
+              {t('author.value')}
+            </td>
+          </tr>
+          <tr>
+            <td style={{ width: '20%' }}>
+              {t('description.title')}
+            </td>
+            <td style={{ width: '5%' }}>:</td>
+            <td style={{ width: '75%' }}>
+              {t('description.value')}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
+
 }
 
-export default App;
+export default withTranslation()(App);
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <I18nextProvider i18n={i18n}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </I18nextProvider>,
   document.getElementById('root')
 );
 
